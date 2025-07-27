e6831ff8bc58bd202e6c44b5392ef9125b472b1d
wxl0430 <141288415+wxl0430@users.noreply.github.com>
2025-07-27 14:03:46 +0800

    feat: 添加了淄博站主要看板

---
+++ Zibo.PrimaryScreen/Views/ScreenView.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using Zibo.PrimaryScreen.ViewModels;
+using System.Windows.Controls;
+namespace Zibo.PrimaryScreen.Views
+{
+    public partial class ScreenView : Page
+    {
+        public ScreenViewModel ViewModel { get; }
+        public ScreenView(ScreenViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+            DataContext = viewModel;
+        }
+    }
+}
